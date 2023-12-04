e869b2620397cbae573e7d898cbe6634687af879
Charles Castillo <castillo089frosty@gmail.com>
2023-12-04 14:40:09 -0500

    change port

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ db.sync()
   .catch(err => console.log(err));
 
 // Spin up server
-const PORT = 3306;
+const PORT = 4000;
 app.listen(PORT, () => {
   console.log(`Express app running on port: ${PORT}`);
 });
